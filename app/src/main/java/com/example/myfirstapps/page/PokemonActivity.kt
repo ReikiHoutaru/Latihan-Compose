b85308ccd6e7609705084644781d74bbb10d6980
Riko <rafarelgibran01@gmail.com>
2025-12-16 15:19:39 +0700

    Membuat Item Dengan Card

--- app/src/main/java/com/example/myfirstapps/page/PokemonActivity.kt
+++ app/src/main/java/com/example/myfirstapps/page/PokemonActivity.kt
@@ -1,27 +1,33 @@
 package com.example.myfirstapps.page
 
 import android.os.Bundle
-import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myfirstapps.R
-import com.example.myfirstapps.model.PokemonModel
 import com.example.myfirstapps.page.ui.theme.MyFirstAppsTheme
 
 class PokemonActivity : ComponentActivity() {
@@ -31,7 +37,7 @@ class PokemonActivity : ComponentActivity() {
         setContent {
             MyFirstAppsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GambarPokemon(Modifier.padding(innerPadding))
+                    PokemonItem(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -39,7 +45,7 @@ class PokemonActivity : ComponentActivity() {
 }
 
 @Composable
-fun GambarPokemon(modifier: Modifier = Modifier){
+fun GambarPokemon(){
     Image(
         painter = painterResource(id = R.drawable.bullbasaur),
         contentDescription = null,
@@ -49,4 +55,37 @@ fun GambarPokemon(modifier: Modifier = Modifier){
             .size(84.dp)
             .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
     )
+}
+
+@Composable
+fun PokemonItem(modifier: Modifier = Modifier){
+    Card(
+        modifier= Modifier
+            .padding(horizontal = 8.dp,8.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(2.dp),
+        colors = CardDefaults.cardColors(contentColor = Color.White),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp))
+    ){
+        Row{
+            GambarPokemon()
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(
+                    text = "Nama Pokemon",
+                    style = typography.headlineMedium ,
+                    color = Color.Black
+                )
+
+                Text(
+                    text = "Detail",
+                    style = typography.bodyMedium,
+                    color = Color.Black)
+            }
+        }
+    }
 }
